937c4db74e5dc11f44c42504e4d4bd3a5aa51c0b
RoshniPreman <roshnipreman92@gmail.com>
2018-04-11 22:43:29 +0530

    Added comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 
 
+//  The CSS selector in the @Component decorator specifies an element named <app-root>. 
+//  That element is a placeholder in the body of your index.html file:
+//  When you bootstrap with the AppComponent class (in main.ts), Angular looks for a <app-root> in the 
+//  index.html, finds it, instantiates an instance of AppComponent, and renders it inside the <app-root> tag.
 
 @Component({      // @component is a decorator function that specifies angular metadata for the component
   selector: 'app-root',
